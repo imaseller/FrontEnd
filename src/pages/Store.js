c503ab3af92a13312fbc152e484e6e2a307ad195
youminki <dbalsrl7648@gmail.com>
2024-08-19 15:33:47 +0900

    fix : Store naming

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../components/Main/Header.js';
-import Items from '../components/Sotre/Items.js';
+import Items from '../components/Store/Items.js';
 import BottomNav from '../components/Main/BottomNav';
 
 const Store = () => {
