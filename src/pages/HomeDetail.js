90e16bcf47c23bba5a4996e15f5380c3be44cc1c
youminki <dbalsrl7648@gmail.com>
2024-08-21 16:51:21 +0900

    fix : SizeInfo 구조 변경

--- src/pages/HomeDetail.js
+++ src/pages/HomeDetail.js
@@ -4,7 +4,6 @@ import Theme from '../styles/Theme.js';
 import ExIMG1 from '../img/Home/ExIMG1.svg';
 import ExIMG2 from '../img/Home/ExIMG2.svg';
 import ExIMG3 from '../img/Home/ExIMG3.svg';
-import SizeInfoImageSrc from '../img/Home/HomeDetail/SizeInfo.svg'; // Import your uploaded image as a source
 
 import Header from '../components/Home/HomeDetail/Header.js';
 import ImageSlider from '../components/Home/HomeDetail/ImageSlider.js';
@@ -12,6 +11,7 @@ import ProductInfo from '../components/Home/HomeDetail/ProductInfo.js';
 import PriceTrendChart from '../components/Home/HomeDetail/PriceTrendChart.js';
 import ProductOptions from '../components/Home/HomeDetail/ProductOptions.js';
 import PaymentMethod from '../components/Home/HomeDetail/PaymentMethod.js';
+import SizeInfo from '../components/Home/HomeDetail/SizeInfo.js'; // Import the SizeInfo component
 
 const HomeDetail = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -91,42 +91,7 @@ const HomeDetail = () => {
         setSelectedColor={setSelectedColor}
       />
       <PriceTrendChart data={priceHistory} />
-      <SizeInfoContainer>
-        <label>사이즈 정보</label>
-        <img src={SizeInfoImageSrc} alt='Size Information' />
-        <TableRow>
-          <TableHeader></TableHeader>
-          <TableHeader>A</TableHeader>
-          <TableHeader>B</TableHeader>
-          <TableHeader>C</TableHeader>
-          <TableHeader>D</TableHeader>
-          <TableHeader>E</TableHeader>
-        </TableRow>
-        <TableRow>
-          <TableCell>S (44)</TableCell>
-          <TableCell>37</TableCell>
-          <TableCell>85</TableCell>
-          <TableCell>72</TableCell>
-          <TableCell>57</TableCell>
-          <TableCell>100</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>M (55)</TableCell>
-          <TableCell>38</TableCell>
-          <TableCell>86</TableCell>
-          <TableCell>74</TableCell>
-          <TableCell>59</TableCell>
-          <TableCell>102</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>L (66)</TableCell>
-          <TableCell>39</TableCell>
-          <TableCell>87</TableCell>
-          <TableCell>80</TableCell>
-          <TableCell>60</TableCell>
-          <TableCell>104</TableCell>
-        </TableRow>
-      </SizeInfoContainer>
+      <SizeInfo /> {/* Use the SizeInfo component */}
       <OrderButton>제품 주문하기</OrderButton>
     </DetailContainer>
   );
@@ -141,44 +106,6 @@ const DetailContainer = styled.div`
   height: 100vh;
   max-width: 600px;
   margin: 0 auto;
-`;
-
-const SizeInfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 20px;
-  border: 1px solid #dddddd;
-  background-color: rgba(255, 255, 255, 0.96);
-  width: 100%;
-`;
-
-const TableRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  padding: 8px 0;
-  border: 1px solid #dddddd;
-`;
-
-const TableHeader = styled.div`
-  font-weight: 900;
-  font-size: 12px;
-  color: #000000;
-  background-color: ${Theme.colors.gray1};
-  flex: 1;
-  text-align: center;
-  padding: 10px 0;
-`;
-
-const TableCell = styled.div`
-  font-weight: 700;
-  font-size: 12px;
-
-  color: #000000;
-  flex: 1;
-  text-align: center;
-  padding: 8px 0;
 `;
 
 const OrderButton = styled.button`
