86bf87f35bf4685b7c67ee064e1a45ae1e6b3f65
youminki <dbalsrl7648@gmail.com>
2024-08-28 13:09:09 +0900

    fix : 유효성 검사 로직 변경

--- src/hooks/ValidationYup.js
+++ src/hooks/ValidationYup.js
@@ -7,25 +7,30 @@ export const schemaSignup = yup.object().shape({
     .min(5, '이메일은 최소 5자 이상 입력해야 합니다.')
     .max(20, '이메일은 최대 20자까지 입력 가능합니다.')
     .matches(/^[a-zA-Z0-9]+$/, '이메일은 영어와 숫자만 입력 가능합니다.'),
+
   password: yup
     .string()
     .required('비밀번호를 입력해주세요.')
     .min(8, '비밀번호는 최소 8자 이상이어야 합니다.')
     .max(20, '비밀번호는 최대 20자까지 입력 가능합니다.')
     .matches(
-      /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]{8,20}$/,
+      /^(?=.*\d)[A-Za-z\d@$!%*?&]{8,20}$/,
       '비밀번호는 영문과 숫자를 포함하여 8~20자 사이로 입력해야 합니다.'
     ),
+
   passwordConfirm: yup
     .string()
-    .oneOf([yup.ref('pw'), null], '비밀번호가 일치하지 않습니다.')
+    .oneOf([yup.ref('password'), null], '비밀번호가 일치하지 않습니다.')
     .required('비밀번호 확인을 위해 다시 입력해주세요.')
     .min(8, '비밀번호 확인은 최소 8자 이상이어야 합니다.')
     .max(20, '비밀번호 확인은 최대 20자까지 입력 가능합니다.'),
+
   nickname: yup
     .string()
     .required('닉네임을 입력해주세요.')
-    .max(8, '닉네임은 최대 8자까지 입력 가능합니다.'),
+    .max(8, '닉네임은 최대 8자까지 입력 가능합니다.')
+    .matches(/^[가-힣a-zA-Z0-9]{1,8}$/, '올바른 닉네임을 입력하세요.'),
+
   instar: yup
     .string()
     .required('인스타그램 아이디를 입력해주세요.')
@@ -35,12 +40,18 @@ export const schemaSignup = yup.object().shape({
       /^[a-zA-Z0-9]+$/,
       '인스타그램 아이디는 영어와 숫자만 입력 가능합니다.'
     ),
+
   name: yup
     .string()
     .required('이름을 입력해주세요.')
-    .max(5, '이름은 최대 5자까지 입력 가능합니다.')
-    .matches(/^[가-힣]+$/, '이름은 한국어만 입력 가능합니다.'),
-  phoneNumber: yup.string().required('전화번호를 입력해주세요.'),
+    .max(6, '이름은 최대 6자까지 입력 가능합니다.')
+    .matches(/^[가-힣]+$/, '올바른 이름을 입력하세요.'),
+
+  phoneNumber: yup
+    .string()
+    .required('전화번호를 입력해주세요.')
+    .min(11, '전화번호는 11자를 입력해야 합니다.'),
+
   melpickAddress: yup
     .string()
     .required('멜픽 주소를 입력해주세요.')
