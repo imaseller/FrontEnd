41737c5a7ba1f274d61e554602d0fe5f04c03477
youminki <dbalsrl7648@gmail.com>
2024-09-06 03:03:03 +0900

    feat : AgreementSection

--- src/components/inventory/AgreementSection.js
+++ src/components/inventory/AgreementSection.js
@@ -1,42 +1,31 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import Theme from '../../styles/Theme';
 
 const AgreementSection = () => {
+  const [allChecked, setAllChecked] = useState(false);
   const [individualChecks, setIndividualChecks] = useState({
     agree1: false,
     agree2: false,
   });
 
   const [modalVisible, setModalVisible] = useState(false);
-  const [cancelConfirmationVisible, setCancelConfirmationVisible] =
-    useState(false);
   const [modalContent, setModalContent] = useState('');
-  const [brands, setBrands] = useState([
-    { name: 'CC Collect', selected: false },
-    { name: 'CLUB Monaco', selected: false },
-    { name: 'DECO', selected: false },
-    { name: 'DEW L', selected: false },
-    { name: 'EGOIST', selected: false },
-    { name: 'it MICHAA', selected: false },
-    { name: 'JIGOTT', selected: false },
-    { name: 'JJ JIGOTT', selected: false },
-    { name: 'KENNETH LADY', selected: false },
-    { name: 'LÄTT BY T', selected: false },
-    { name: 'LINE', selected: false },
-  ]);
 
-  const [warningModalVisible, setWarningModalVisible] = useState(false);
+  const handleAllChecked = () => {
+    const newValue = !allChecked;
+    setAllChecked(newValue);
+    setIndividualChecks({
+      agree1: newValue,
+      agree2: newValue,
+    });
+  };
 
-  const handleBrandSelect = (index) => {
-    const selectedCount = brands.filter((brand) => brand.selected).length;
-    if (selectedCount < 3 || brands[index].selected) {
-      setBrands((prevBrands) =>
-        prevBrands.map((brand, i) =>
-          i === index ? { ...brand, selected: !brand.selected } : brand
-        )
-      );
-    }
+  const handleIndividualCheck = (e) => {
+    const { id, checked } = e.target;
+    setIndividualChecks((prev) => ({
+      ...prev,
+      [id]: checked,
+    }));
   };
 
   const handleViewDetails = (content) => {
@@ -48,34 +37,18 @@ const AgreementSection = () => {
     setModalVisible(false);
   };
 
-  const handleCompleteSelection = () => {
-    const selectedCount = brands.filter((brand) => brand.selected).length;
-    if (selectedCount !== 3) {
-      setWarningModalVisible(true);
-    } else {
-      closeModal();
-    }
-  };
-
-  const closeWarningModal = () => {
-    setWarningModalVisible(false);
-  };
-
-  const handleCancelSelection = () => {
-    setCancelConfirmationVisible(true);
-  };
-
-  const confirmCancel = () => {
-    setCancelConfirmationVisible(false);
-    closeModal();
-  };
-
-  const declineCancel = () => {
-    setCancelConfirmationVisible(false);
-  };
-
   return (
     <AgreementWrapper>
+      <AllAgreeWrapper>
+        <Checkbox
+          type='checkbox'
+          id='agreeAll'
+          checked={allChecked}
+          onChange={handleAllChecked}
+        />
+        <Label htmlFor='agreeAll'>전체동의</Label>
+      </AllAgreeWrapper>
+
       <ContentContainer>
         <CheckboxWrapper>
           <Checkbox
@@ -83,23 +56,44 @@ const AgreementSection = () => {
             id='agree1'
             required
             checked={individualChecks.agree1}
-            onChange={(e) =>
-              setIndividualChecks({
-                ...individualChecks,
-                agree1: e.target.checked,
-              })
-            }
+            onChange={handleIndividualCheck}
           />
           <Label htmlFor='agree1'>
-            정보입력 동의 <RequiredText>(필수)</RequiredText>
+            이용약관 동의 <RequiredText>(필수)</RequiredText>
           </Label>
         </CheckboxWrapper>
         <InputWrapper>
           <DescriptionWrapper>
-            <Description>설정에 필요한 정보입력 동의 안내.</Description>
+            <Description>이용 전 필수사항 및 주의사항 안내.</Description>
           </DescriptionWrapper>
           <ViewDetailsButton
-            onClick={() => handleViewDetails('내용을 입력하세요.')}
+            onClick={() =>
+              handleViewDetails(`
+                본 약관은 주식회사 멜픽(이하 "회사"라 합니다.)가 제공하는 의류 및 잡화(이하 "제품"이라 합니다.) 판매 및 전자상거래에 관한 온/오프라인상의 제반 서비스(이하 "서비스"라 합니다.)를 이용함에 있어서 회사와 회원의 권리와 의무에 대한 책임사항을 규정함을 목적으로 합니다.
+              `)
+            }
+          >
+            전체보기
+          </ViewDetailsButton>
+        </InputWrapper>
+        <CheckboxWrapper>
+          <Checkbox
+            type='checkbox'
+            id='agree2'
+            required
+            checked={individualChecks.agree2}
+            onChange={handleIndividualCheck}
+          />
+          <Label htmlFor='agree2'>
+            개인정보수집 동의 <RequiredText>(필수)</RequiredText>
+          </Label>
+        </CheckboxWrapper>
+        <InputWrapper>
+          <DescriptionWrapper>
+            <Description>서비스 이용에 필요한 개인정보 수집 안내.</Description>
+          </DescriptionWrapper>
+          <ViewDetailsButton
+            onClick={() => handleViewDetails('개인정보수집 내용')}
           >
             전체보기
           </ViewDetailsButton>
@@ -111,70 +105,19 @@ const AgreementSection = () => {
           <ModalContent>
             <ContentWrapper>
               <ModalHeader>
-                <ModalTitle>
-                  브랜드 선택 <GrayText>(3가지 선택)</GrayText>
-                </ModalTitle>
+                <ModalTitle>이용약관</ModalTitle>
                 <GrayLine />
+                <SectionTitle>제1장 총칙</SectionTitle>
+                <SubTitle>제1조 (목적)</SubTitle>
               </ModalHeader>
-
-              <ModalBody>
-                <BrandSelectionGrid>
-                  {brands.map((brand, index) => (
-                    <BrandOption
-                      key={index}
-                      selected={brand.selected}
-                      onClick={() => handleBrandSelect(index)}
-                    >
-                      {brand.name}
-                    </BrandOption>
-                  ))}
-                </BrandSelectionGrid>
-              </ModalBody>
+              <Text>{modalContent}</Text>
               <GrayLine />
-              <ButtonRow>
-                <CancelButton onClick={handleCancelSelection}>
-                  취소
-                </CancelButton>
-                <CompleteButton onClick={handleCompleteSelection}>
-                  선택완료
-                </CompleteButton>
-              </ButtonRow>
+              <CloseButtonWrapper>
+                <CloseButton onClick={closeModal}>확인</CloseButton>
+              </CloseButtonWrapper>
             </ContentWrapper>
           </ModalContent>
         </Modal>
-      )}
-
-      {warningModalVisible && (
-        <WarningModal>
-          <WarningModalContent>
-            <ModalHeader>
-              <ModalTitle>알림</ModalTitle>
-              <GrayLine />
-            </ModalHeader>
-            <WarningMessage>3가지를 선택해야 합니다!</WarningMessage>
-            <GrayLine />
-            <ButtonRow>
-              <CancelButton onClick={closeWarningModal}>닫기</CancelButton>
-            </ButtonRow>
-          </WarningModalContent>
-        </WarningModal>
-      )}
-
-      {cancelConfirmationVisible && (
-        <WarningModal>
-          <WarningModalContent>
-            <ModalHeader>
-              <ModalTitle>알림</ModalTitle>
-              <GrayLine />
-            </ModalHeader>
-            <WarningMessage>설정을 취소하시겠습니까?</WarningMessage>
-            <GrayLine />
-            <ButtonRow>
-              <CancelButton onClick={declineCancel}>아니요</CancelButton>
-              <CompleteButton onClick={confirmCancel}>네</CompleteButton>
-            </ButtonRow>
-          </WarningModalContent>
-        </WarningModal>
       )}
     </AgreementWrapper>
   );
@@ -183,8 +126,14 @@ const AgreementSection = () => {
 export default AgreementSection;
 
 const AgreementWrapper = styled.div`
-  background-color: ${Theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.white};
   margin-bottom: 20px;
+`;
+
+const AllAgreeWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
 `;
 
 const CheckboxWrapper = styled.div`
@@ -194,40 +143,45 @@ const CheckboxWrapper = styled.div`
 `;
 
 const ContentContainer = styled.div`
-  background-color: ${Theme.colors.lightgray};
-  border: 1px solid ${Theme.colors.gray0};
+  background-color: ${({ theme }) => theme.colors.lightgray};
+  border: 1px solid ${({ theme }) => theme.colors.gray0};
   padding: 20px;
   width: 100%;
 `;
 
 const InputWrapper = styled.div`
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
   align-items: center;
   margin-bottom: 20px;
-  background-color: ${Theme.colors.white};
-  border: 1px solid ${Theme.colors.gray3};
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.gray3};
   padding: 10px;
+  position: relative;
 `;
 
 const Checkbox = styled.input`
+  margin-bottom: 5px;
   width: 20px;
   height: 20px;
   margin-right: 10px;
   appearance: none;
-  background-color: ${Theme.colors.white};
-  border: 1px solid ${Theme.colors.gray2};
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.gray2};
   border-radius: 3px;
   cursor: pointer;
 
   &:checked {
-    background-color: ${Theme.colors.white};
-    border-color: ${Theme.colors.gray1};
+    background-color: ${({ theme }) => theme.colors.white};
+    border-color: ${({ theme }) => theme.colors.gray1};
   }
 
   &:checked::before {
     content: '✔';
-    color: ${Theme.colors.yellow};
+    color: ${({ theme }) => theme.colors.yellow};
     display: flex;
     align-items: center;
     justify-content: center;
@@ -235,24 +189,26 @@ const Checkbox = styled.input`
 `;
 
 const Label = styled.label`
-  ${Theme.fonts.default};
-  color: ${Theme.colors.black};
+  ${({ theme }) => theme.fonts.default};
+  color: ${({ theme }) => theme.colors.black};
 `;
 
 const RequiredText = styled.span`
-  color: ${Theme.colors.gray2};
+  color: ${({ theme }) => theme.colors.gray2};
 `;
 
 const ViewDetailsButton = styled.button`
   width: 69px;
   height: 34px;
-  background-color: ${Theme.colors.black};
-  color: ${Theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.black};
+  color: ${({ theme }) => theme.colors.white};
   border: none;
   cursor: pointer;
   font-size: 12px;
   border-radius: 5px;
+  font-style: normal;
   font-weight: 800;
+  text-align: center;
 `;
 
 const DescriptionWrapper = styled.div`
@@ -263,10 +219,12 @@ const DescriptionWrapper = styled.div`
 `;
 
 const Description = styled.p`
-  color: ${Theme.colors.gray};
+  color: ${({ theme }) => theme.colors.gray};
   margin: 0;
+  font-style: normal;
   font-weight: 700;
   font-size: 12px;
+  line-height: 13px;
 `;
 
 const Modal = styled.div`
@@ -283,21 +241,20 @@ const Modal = styled.div`
 `;
 
 const ModalContent = styled.div`
-  background-color: ${Theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.white};
   padding: 20px;
   max-width: 500px;
   width: 100%;
   height: 670px;
+  text-align: left;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: space-between;
 `;
 
 const ContentWrapper = styled.div`
   flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  height: 486px;
 `;
 
 const ModalHeader = styled.div`
@@ -309,120 +266,55 @@ const ModalTitle = styled.p`
   font-family: 'NanumSquare Neo OTF', sans-serif;
   font-weight: 800;
   font-size: 16px;
+  line-height: 16px;
   margin-top: 20px;
 `;
 
-const GrayText = styled.span`
-  color: ${Theme.colors.gray1};
+const SectionTitle = styled.p`
+  font-family: 'NanumSquare Neo OTF', sans-serif;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 13.26px;
+  margin-bottom: 20px;
 `;
 
-const ModalBody = styled.div`
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+const SubTitle = styled.p`
+  font-family: 'NanumSquare Neo OTF', sans-serif;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 13.26px;
+  margin-bottom: 20px;
 `;
 
-const BrandSelectionGrid = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-`;
-
-const BrandOption = styled.div`
-  padding: 10px;
-  background-color: ${(props) =>
-    props.selected ? Theme.colors.yellow : Theme.colors.white};
-  color: ${(props) =>
-    props.selected ? Theme.colors.white : Theme.colors.black};
-  border: 1px solid ${Theme.colors.gray1};
-  text-align: center;
-  cursor: pointer;
-  font-weight: bold;
-
-  white-space: nowrap;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const Text = styled.p`
+  height: 386px;
+  font-family: 'NanumSquare Neo OTF', sans-serif;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 13.26px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.colors.gray2};
 `;
 
 const GrayLine = styled.hr`
   border: none;
   width: 100%;
-  border: 1px solid ${Theme.colors.gray0};
+  border: 1px solid ${({ theme }) => theme.colors.gray0};
   margin: 20px 0;
 `;
 
-const ButtonRow = styled.div`
+const CloseButtonWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
-  gap: 20px;
-  margin-top: auto;
-`;
-
-const CancelButton = styled.button`
-  width: 100%;
-  height: 56px;
-  background-color: ${Theme.colors.gray1};
-  color: ${Theme.colors.white};
-  border: none;
-  border-radius: 6px;
-  cursor: pointer;
-
-  font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
-  font-weight: 800;
-  font-size: 16px;
-  line-height: 18px;
-
-  text-align: center;
-
-  color: #ffffff;
-`;
-
-const CompleteButton = styled.button`
-  width: 100%;
-  height: 56px;
-  background-color: ${Theme.colors.black};
-  color: ${Theme.colors.white};
-  border: none;
-  border-radius: 6px;
-  cursor: pointer;
-
-  font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
-  font-weight: 800;
-  font-size: 16px;
-  line-height: 18px;
-  text-align: center;
-
-  color: #ffffff;
-`;
-
-const WarningModal = styled(Modal)`
-  background-color: rgba(0, 0, 0, 0.7);
-  width: 100%;
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 0 27px;
-`;
-
-const WarningModalContent = styled(ModalContent)`
-  max-width: 376px;
-  height: 329px;
-  display: flex;
-  flex-direction: column;
   justify-content: center;
 `;
 
-const WarningMessage = styled.p`
-  color: ${Theme.colors.black};
-  font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 15px;
-  text-align: center;
-  justify-content: center;
-  margin: 0;
+const CloseButton = styled.button`
+  width: 100%;
+  height: 56px;
+  background-color: #000000;
+  color: #ffffff;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  font-size: 16px;
 `;
