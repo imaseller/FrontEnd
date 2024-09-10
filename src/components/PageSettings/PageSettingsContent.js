4c41e7551b4a13cfa8116b1f1b9ef7aae96aa170
youminki <dbalsrl7648@gmail.com>
2024-09-10 17:12:02 +0900

    fix: 페이지 셋팅 구조 및 css #1

--- src/components/PageSettings/PageSettingsContent.js
+++ src/components/PageSettings/PageSettingsContent.js
@@ -1,24 +1,192 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaChevronRight } from 'react-icons/fa';
 import Theme from '../../styles/Theme';
 
-const PageSettingsContent = () => {
+const PageSettingsContent = ({
+  profileName,
+  eventDate,
+  eventPeriod,
+  eventDescription,
+  items,
+}) => {
   return (
     <ContentContainer>
-      <h1>유형 분석</h1>
-      <p>여기에서 사용자의 유형을 분석할 수 있습니다.</p>
+      <Header>
+        <ProfileSection>
+          <ProfileImage />
+          <ProfileInfo>
+            <ProfileName>{profileName}</ProfileName>
+          </ProfileInfo>
+        </ProfileSection>
+        <EventSection>
+          <EventBox>
+            <EventRow>
+              <EventDate>{eventDate}</EventDate>
+              <EventText>
+                <EventPeriod>{eventPeriod}</EventPeriod>
+                <EventDescription>{eventDescription}</EventDescription>
+              </EventText>
+            </EventRow>
+          </EventBox>
+        </EventSection>
+      </Header>
+
+      <Description>👉 직접 입어보고 맘에 드는 것만 소개해드려요 👈</Description>
+
+      <ItemList>
+        {items.map((item, index) => (
+          <Item key={index}>
+            <ItemImage src={item.imageSrc} alt={item.text} />
+            <ItemText>{item.text}</ItemText>
+            <ArrowIcon />
+          </Item>
+        ))}
+      </ItemList>
     </ContentContainer>
   );
 };
 
 export default PageSettingsContent;
 
+// Styled Components
 const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 600px;
   margin: 0 auto;
   width: 100%;
-  padding: 125px 27px 0 27px;
+  padding: 20px;
   border: 1px solid ${Theme.colors.gray1};
+  background-color: ${Theme.colors.white};
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0;
+`;
+
+const ProfileSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProfileImage = styled.div`
+  width: 60px;
+  height: 60px;
+  background-color: ${Theme.colors.gray3};
+  border-radius: 50%;
+  margin-bottom: 10px;
+`;
+
+const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProfileName = styled.h1`
+  font-size: 18px;
+  font-weight: bold;
+  margin: 0;
+`;
+
+const EventSection = styled.div`
+  margin-top: 20px;
+`;
+
+const EventBox = styled.div`
+  width: 375px;
+  height: 80px;
+  border: 0.5px solid #f6ae24;
+  border-radius: 10px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start; /* 왼쪽 상단에 정렬 */
+`;
+
+const EventRow = styled.div`
+  display: flex;
+  align-items: flex-start; /* 왼쪽 상단 정렬 */
+  width: 100%;
+`;
+
+const EventDate = styled.span`
+  color: #ffffff;
+  background: #000000;
+  padding: 5px 10px;
+  width: 65px;
+  margin-right: 10px;
+  clip-path: polygon(0 0, 80% 0, 90% 50%, 80% 100%, 0 100%);
+
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 10px;
+  line-height: 12px;
+`;
+
+const EventText = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const EventPeriod = styled.span`
+  font-size: 14px;
+  color: ${Theme.colors.gray2};
+`;
+
+const EventDescription = styled.span`
+  font-size: 14px;
+  color: ${Theme.colors.black};
+  margin-top: 5px;
+`;
+
+const Description = styled.p`
+  text-align: center;
+  font-size: 14px;
+  padding: 20px 0;
+  color: ${Theme.colors.black};
+`;
+
+const ItemList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+  width: 375px;
+  height: 70px;
+  padding: 10px;
+  border: 0.5px solid #cccccc;
+  border-radius: 35px;
+  margin: 0 auto;
+`;
+
+const ItemImage = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-right: 20px;
+  border-radius: 50%;
+  background-color: ${Theme.colors.gray3};
+`;
+
+const ItemText = styled.span`
+  font-size: 14px;
+  color: ${Theme.colors.black};
+  flex: 1;
+`;
+
+const ArrowIcon = styled(FaChevronRight)`
+  color: ${Theme.colors.gray2};
+  font-size: 19px;
 `;
--- src/pages/PageSettings.js
+++ src/pages/PageSettings.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from '../components/Header.js';
 import Content from '../components/PageSettings/PageSettingsContent.js';
 import BottomNav from '../components/Home/BottomNav';
 import Theme from '../styles/Theme';
+import ExIMG1 from '../img/Home/ExIMG1.svg';
 
 const PageSettings = () => {
+  const data = {
+    profileName: 'Mr J',
+    eventDate: 'D-DAY',
+    eventPeriod: '09.03(오전 0시) - 09.05(오전 0시)',
+    eventDescription: 'SANDRO 언발 플레어 미니원피스 공구',
+    items: [
+      { imageSrc: ExIMG1, text: 'SANDRO 언발 플레어 미니원피스' },
+      { imageSrc: ExIMG1, text: 'ZOOC 볼륨소매 랩 카라 블라우스' },
+      {
+        imageSrc: ExIMG1,
+        text: 'MICHA 테일러드 카라 머메이드 원피스',
+      },
+      {
+        imageSrc: ExIMG1,
+        text: 'CC Collect 프릴 디테일 로우웨이스트 원피스',
+      },
+    ],
+  };
+
   return (
     <AnalysisContainer>
       <ContentWrapper>
-        <Header />
-        <Content />
+        <Content
+          profileName={data.profileName}
+          eventDate={data.eventDate}
+          eventPeriod={data.eventPeriod}
+          eventDescription={data.eventDescription}
+          items={data.items}
+        />
       </ContentWrapper>
       <BottomNav />
     </AnalysisContainer>
@@ -19,6 +43,7 @@ const PageSettings = () => {
 
 export default PageSettings;
 
+// Styled Components
 const AnalysisContainer = styled.div`
   display: flex;
   flex-direction: column;
