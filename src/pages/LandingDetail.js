d51a105c50ff0a8e3872caaec06f0b482c5924c1
youminki <dbalsrl7648@gmail.com>
2024-09-26 16:05:20 +0900

    feat : LandingDetail 4페이지 UI

--- src/pages/LandingDetail.js
+++ src/pages/LandingDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import Theme from '../styles/Theme';
 import HangerIcon from '../img/Landing/hangerIcon.svg';
@@ -11,15 +11,42 @@ import ServiceImg3 from '../img/Landing/ServiceImg3.svg';
 import ScreenImg1 from '../img/Landing/ScreenImg1.svg';
 import ScreenImg2 from '../img/Landing/ScreenImg2.svg';
 import ScreenImg3 from '../img/Landing/ScreenImg3.svg';
+import LeftArrowIcon from '../img/Landing/left-arrow.svg';
+import RightArrowIcon from '../img/Landing/right-arrow.svg';
 
 const LandingDetail = () => {
+  const [currentScreenIndex, setCurrentScreenIndex] = useState(0);
+
+  const screens = [
+    {
+      img: ScreenImg1,
+      subtitle: (
+        <>
+          인스타그램으로 나의 스타일 파악 후 <br />
+          브랜드 매칭하기
+        </>
+      ),
+    },
+    { img: ScreenImg2, subtitle: '노출 & 포스팅할 제품 선택하기' },
+    {
+      img: ScreenImg3,
+      subtitle: '다양한 브랜드 상품과 나만의 스타일 완성하기',
+    },
+  ];
+
+  const handlePrevClick = () =>
+    setCurrentScreenIndex((prevIndex) =>
+      prevIndex === 0 ? screens.length - 1 : prevIndex - 1
+    );
+  const handleNextClick = () =>
+    setCurrentScreenIndex((prevIndex) =>
+      prevIndex === screens.length - 1 ? 0 : prevIndex + 1
+    );
+
   return (
     <ThemeProvider theme={Theme}>
       <Container>
-        <ScrollIndicator>
-          <Arrow>↓</Arrow>
-          <span>scroll</span>
-        </ScrollIndicator>
+        <ScrollIndicator />
         <Hanger src={HangerIcon} alt='hanger icon' />
         <LandingTitle1>
           당신의 취향에 꼭 맞는 <br />
@@ -27,48 +54,34 @@ const LandingDetail = () => {
           <MelpicHighlight>멜픽과 함께합니다!</MelpicHighlight>
         </LandingTitle1>
         <BrandList>
-          <Brand>
-            <BrandImage src={ZOOCImage} alt='ZOOC' />
-            <BrandName>ZOOC</BrandName>
-          </Brand>
-          <Brand>
-            <BrandImage src={SANDROImage} alt='SANDRO' />
-            <BrandName>SANDRO</BrandName>
-          </Brand>
-          <Brand>
-            <BrandImage src={ITMICHAImage} alt='it MICHA' />
-            <BrandName>it MICHA</BrandName>
-          </Brand>
+          {[ZOOCImage, SANDROImage, ITMICHAImage].map((img, idx) => (
+            <Brand key={idx}>
+              <BrandImage src={img} alt={`brand-${idx}`} />
+              <BrandName>{['ZOOC', 'SANDRO', 'it MICHA'][idx]}</BrandName>
+            </Brand>
+          ))}
         </BrandList>
         <Footer>
-          똑똑한 AI가 <br />
-          당신도 몰랐던, 당신만의 스타일을 <br />
-          매칭해드립니다.
+          똑똑한 AI가 당신도 몰랐던, 당신만의 스타일을 매칭해드립니다.
         </Footer>
       </Container>
 
-      <Container style={{ backgroundColor: 'white' }}>
+      <Container bgColor='white'>
         <LandingTitle2>
           오직 나만의, 나를 위한 상품
-          <br />
           <LandingSubtitle2>
             이제 일일이 찾지 마세요
             <br />
-            브랜드는 <YellowText>멜픽 pick 해줄게요</YellowText>
+            브랜드는 <YellowText>멜픽이 pick 해줄게요!</YellowText>
           </LandingSubtitle2>
         </LandingTitle2>
 
-        {/* Images arranged in a column */}
         <ColumnServiceList>
-          <Service>
-            <ServiceImg src={ServiceImg1} alt='ZOOC' />
-          </Service>
-          <Service>
-            <ServiceImg src={ServiceImg2} alt='SANDRO' />
-          </Service>
-          <Service>
-            <ServiceImg src={ServiceImg3} alt='it MICHA' />
-          </Service>
+          {[ServiceImg1, ServiceImg2, ServiceImg3].map((img, idx) => (
+            <Service key={idx}>
+              <ServiceImg src={img} alt={`service-${idx}`} />
+            </Service>
+          ))}
         </ColumnServiceList>
       </Container>
 
@@ -77,20 +90,25 @@ const LandingDetail = () => {
           <MelpicHighlight>melpik</MelpicHighlight>은 <br />
           <BrandHighlight>어떤 서비스인가요?</BrandHighlight> <br />
           <LandingSubtitle3>
-            인스타그램으로 나의 스타일 파악 후 <br /> 브랜드 매칭하기
+            {screens[currentScreenIndex].subtitle}
           </LandingSubtitle3>
         </LandingTitle3>
-        <ScreenList>
-          <Screen>
-            <ScreenImage src={ScreenImg1} alt='ZOOC' />
-          </Screen>
-          <Screen>
-            <ScreenImage src={ScreenImg2} alt='ZOOC' />
-          </Screen>
-          <Screen>
-            <ScreenImage src={ScreenImg3} alt='ZOOC' />
-          </Screen>
-        </ScreenList>
+        <ScreenContainer>
+          <ArrowButton onClick={handlePrevClick}>
+            <ArrowIcon src={LeftArrowIcon} alt='Previous' />
+          </ArrowButton>
+          <ScreenImageContainer>
+            <ScreenImage src={screens[currentScreenIndex].img} alt='Screen' />
+          </ScreenImageContainer>
+          <ArrowButton onClick={handleNextClick}>
+            <ArrowIcon src={RightArrowIcon} alt='Next' />
+          </ArrowButton>
+        </ScreenContainer>
+        <PaginationDots>
+          {screens.map((_, idx) => (
+            <Dot key={idx} isActive={currentScreenIndex === idx} />
+          ))}
+        </PaginationDots>
       </Container>
     </ThemeProvider>
   );
@@ -109,8 +127,8 @@ const Container = styled.div`
   justify-content: flex-start;
   padding: 20px;
   height: 100vh;
-  border: 1px solid ${Theme.colors.gray1};
-  background-color: ${({ theme }) => theme.colors.yellow5};
+  border: 1px solid ${({ theme }) => theme.colors.gray1};
+  background-color: ${({ bgColor, theme }) => bgColor || theme.colors.yellow5};
 `;
 
 const ScrollIndicator = styled.div`
@@ -122,27 +140,28 @@ const ScrollIndicator = styled.div`
   color: ${({ theme }) => theme.colors.gray2};
   height: 50px;
   margin-bottom: 130px;
-`;
-
-const Arrow = styled.div`
-  font-size: 18px;
+  &::before {
+    content: '↓';
+    font-size: 18px;
+  }
+  &::after {
+    content: 'scroll';
+    margin-top: 8px;
+  }
 `;
 
 const Hanger = styled.img`
   width: 40px;
   height: 40px;
-
   margin-bottom: 41px;
 `;
 
 const LandingTitle1 = styled.h1`
   font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
   font-weight: 400;
   font-size: 20px;
   line-height: 40px;
   text-align: center;
-
   margin-bottom: 50px;
 `;
 
@@ -152,24 +171,27 @@ const BrandHighlight = styled.span`
 `;
 
 const MelpicHighlight = styled.span`
-  font-size: 23px;
   color: ${({ theme }) => theme.colors.yellow};
   font-weight: 500;
+
+  font-family: 'NanumSquare Neo OTF';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 25px;
+  line-height: 40px;
+  /* or 160% */
+  text-align: center;
 `;
 
 const BrandList = styled.div`
   display: flex;
   flex-direction: row;
   overflow-x: scroll;
-  scroll-behavior: smooth;
   width: 100%;
   margin-bottom: 20px;
-
   &::-webkit-scrollbar {
     display: none;
   }
-  -ms-overflow-style: none;
-  scrollbar-width: none;
 `;
 
 const Brand = styled.div`
@@ -192,58 +214,40 @@ const BrandName = styled.span`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-
-  font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
   font-weight: 900;
   font-size: 20px;
-  line-height: 22px;
   text-align: center;
 `;
 
 const Footer = styled.div`
   font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
   font-weight: 400;
   font-size: 17px;
   line-height: 30px;
-  /* or 176% */
   text-align: center;
-
   margin-top: 90px;
 `;
 
 const LandingTitle2 = styled.h1`
   font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
   font-weight: 700;
   font-size: 20px;
-  line-height: 25px;
-  /* identical to box height, or 125% */
   text-align: center;
-
   margin-top: 95px;
   margin-bottom: 21px;
 `;
 
 const LandingSubtitle2 = styled.h1`
   font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
   font-weight: 400;
   font-size: 17px;
-  line-height: 25px;
   text-align: center;
-
   margin-top: 21px;
 `;
 
 const YellowText = styled.span`
-  font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
   font-weight: 700;
   font-size: 17px;
-  line-height: 25px;
-  text-align: center;
   color: ${({ theme }) => theme.colors.yellow};
 `;
 
@@ -269,35 +273,15 @@ const Service = styled.div`
   position: relative;
 `;
 
-const LandingTitle3 = styled.h1`
-  font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 25px;
-  /* identical to box height, or 125% */
-  text-align: center;
-
-  margin-top: 95px;
-  margin-bottom: 21px;
+const ScreenContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  position: relative;
 `;
 
-const LandingSubtitle3 = styled.h1`
-  font-family: 'NanumSquare Neo OTF';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 17px;
-  line-height: 25px;
-  text-align: center;
-
-  margin-top: 21px;
-`;
-
-const ScreenImage = styled.img`
-  width: 300px;
-  height: 190px;
-  object-fit: cover;
-  border-radius: 20px;
+const ScreenImageContainer = styled.div`
   width: 220px;
   height: 466px;
   left: calc(50% - 220px / 2 - 480px);
@@ -308,25 +292,70 @@ const ScreenImage = styled.img`
   border-radius: 10px;
 `;
 
-const Screen = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-right: 20px;
-  position: relative;
+const ScreenImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 
-const ScreenList = styled.div`
-  display: flex;
-  flex-direction: row;
-  overflow-x: scroll;
-  scroll-behavior: smooth;
-  width: 100%;
-  margin-bottom: 20px;
+const ArrowButton = styled.button`
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  padding: 10px;
+  z-index: 1;
 
-  &::-webkit-scrollbar {
-    display: none;
+  &:first-of-type {
+    left: 0;
   }
-  -ms-overflow-style: none;
-  scrollbar-width: none;
+
+  &:last-of-type {
+    right: 0;
+  }
+`;
+
+const ArrowIcon = styled.img`
+  width: 11.64px;
+  height: 19.44px;
+`;
+
+const LandingSubtitle3 = styled.h1`
+  font-family: 'NanumSquare Neo OTF';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 17px;
+  line-height: 30px;
+  /* or 176% */
+  text-align: center;
+
+  margin-top: 50px;
+`;
+const LandingTitle3 = styled.h1`
+  font-family: 'NanumSquare Neo OTF';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 25px;
+  text-align: center;
+
+  margin-top: 95px;
+  margin-bottom: 34px;
+`;
+
+const PaginationDots = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+  gap: 10px;
+`;
+
+const Dot = styled.div`
+  width: ${(props) => (props.isActive ? '20px' : '10px')};
+  height: 10px;
+  background-color: ${(props) => (props.isActive ? '#F5AB35' : '#D9D9D9')};
+  border-radius: 100px;
 `;
