f3f804e77da80a44d20a19850488af7b0a3bde84
youminki <dbalsrl7648@gmail.com>
2024-10-08 14:01:46 +0900

    fix : 헤더고정 및 로고 크기 수정

--- src/components/Landing/LandingPage1.js
+++ src/components/Landing/LandingPage1.js
@@ -49,6 +49,18 @@ const LandingPage1 = () => {
 
 export default LandingPage1;
 
+const Header = styled.header`
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  z-index: 10;
+  max-width: 600px;
+  margin-top: 41px;
+`;
+
 const Container = styled.div`
   position: relative;
   display: flex;
@@ -56,6 +68,7 @@ const Container = styled.div`
   align-items: center;
   width: 100%;
   padding: 2rem;
+  padding-top: 80px;
   background-color: transparent;
   min-height: 900px;
   max-width: 600px;
@@ -97,23 +110,17 @@ const BackgroundImage = styled.img`
   z-index: -1;
 `;
 
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  padding: 1rem 4rem;
-`;
-
 const Logo = styled.img`
-  width: 5rem;
-  height: auto;
+  width: 83px;
+  height: 35px;
+  margin-left: 29px;
 `;
 
 const RegisterButton = styled.button`
   padding: 10px 15px;
   background-color: ${({ theme }) => theme.colors.white};
   border-radius: 20px;
+  margin-right: 29px;
   cursor: pointer;
   font-family: 'NanumSquare Neo OTF';
   font-weight: 700;
